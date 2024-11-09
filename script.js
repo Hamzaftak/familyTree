e11435fe817da6837bacfd978de334a3a40f7fb1
Hamza Atakroury <44806256+Hamzaftak@users.noreply.github.com>
2024-11-09 08:55:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 $(document).ready(function () {
-  $("select")
-    .change(function () {
-      $(this)
-        .find("option:selected")
-        .each(function () {
-          var optionValue = $(this).attr("value");
-          if (optionValue) {
-            $(".box")
-              .not("." + optionValue)
-              .hide();
-            $("." + optionValue).show();
-          } else {
-            $(".box").hide();
-          }
-        });
-    })
-    .change();
+  $(".box").hide();
+  $("select").change(function () {
+    $(".firstBox").hide();
+    $(this)
+      .find("option:selected")
+      .each(function () {
+        var optionValue = $(this).attr("value");
+        if (optionValue) {
+          $(".box")
+            .not("." + optionValue)
+            .hide();
+          $("." + optionValue).show();
+        } else {
+          $(".box").hide();
+        }
+      });
+  });
+  //.change();
 });
 
 //console.log(jQuery('span').attr('class'));
 
 $("span.male").click(function () {
+  $(".firstBox").hide();
   var myclass = $(this).prop("class").split(" ")[0];
   if (myclass) {
     $(".box")
